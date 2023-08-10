57b5c0ebe7e47fb18e056c1558bbff0cb347bbc5
edupreto <avelino.edu@gmail.com>
2023-08-10 19:06:45 -0300

    feat(project/slide): change in visualization

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -5,31 +5,37 @@ class MobileNavbar{
     this.navLinks = document.querySelectorAll(navLinks);
     this.activeClass = "active";
     this.handleClick = this.handleClick.bind(this);
+    
   }
 
   animateLinks() {
     this.navLinks.forEach((link, index) => {
-      
       link.style.animation 
         ? (link.style.animation = "")
         : (link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.3}s`);
     });
   }
+
+  closeMenu() {
+    this.navList.addEventListener("click", this.handleClick);
+  }
   
   handleClick() {
     this.navList.classList.toggle(this.activeClass);
     this.mobileMenu.classList.toggle(this.activeClass);
-    this.animateLinks();
-  }
+    this.animateLinks();  
+    this.closeMenu(); 
+          
+  }  
 
   addClickEvent() {
-    this.mobileMenu.addEventListener("click", this.handleClick);
-    this.navList.addEventListener("click", this.handleClick);
+    this.mobileMenu.addEventListener("click", this.handleClick);    
   }
 
   init(){
     if (this.mobileMenu) {
       this.addClickEvent();
+      
     }
     return this;
   }
